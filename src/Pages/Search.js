67703b6965b07c2a8be1d19b144fcfd1a0a0c53d
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-18 18:49:34 -0300

    adiciona requisito 5

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,11 +2,44 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    bandName: '',
+    disabled: true,
+  }
+
+  getBandName = (event) => {
+    const { value } = event.target;
+    const minValue = 2;
+    this.setState({ bandName: value }, () => (
+      value.length >= minValue ? this.setState({ disabled: false }) : (
+        this.setState({ disabled: true })
+      )));
+  }
+
   render() {
+    const { bandName, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search Component</p>
+        <form>
+          <label htmlFor="name">
+            Nome:
+            <input
+              id="name"
+              type="text"
+              value={ bandName }
+              data-testid="search-artist-input"
+              onChange={ this.getBandName }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>);
   }
 }

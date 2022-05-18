a3a7437739ea67889a909e76da424b281883067a
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-18 15:14:57 -0300

    realiza primeiro requisito, criando as rotas entre os arquivos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Login from './Pages/Login';
+import NotFound from './Pages/NotFound';
+import Profile from './Pages/Profile';
+import ProfileEdit from './Pages/ProfileEdit';
+import Search from './Pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
---
+++ src/Pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>NotFound Component</p>
+      </div>);
+  }
+}
+
+export default NotFound;

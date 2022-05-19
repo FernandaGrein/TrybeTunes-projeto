a2785e0a1ef96516d78296d685d1007a6f81c35d
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-19 14:48:56 -0300

    realiza requisito 8

--- src/Pages/Album.js
+++ src/Pages/Album.js
@@ -1,6 +1,8 @@
 import propTypes from 'prop-types';
 import React from 'react';
 import Header from '../Components/Header';
+import Loading from '../Components/Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
@@ -8,6 +10,7 @@ class Album extends React.Component {
     musicArray: [],
     colectionName: '',
     artistName: '',
+    loading: false,
   }
 
   componentDidMount() {
@@ -18,7 +21,6 @@ class Album extends React.Component {
     const { location: { pathname } } = this.props;
     const id = pathname.split('/album/');
     const music = await getMusics(id[1]);
-    console.log(music);
     this.setState({ musicArray: music }, () => this.getAlbumAndArtistName());
   }
 
@@ -29,11 +31,21 @@ class Album extends React.Component {
       artistName: firstIndex.artistName });
   }
 
+  getTarget = async (event) => {
+    const favSong = event.target;
+    const { musicArray } = this.state;
+    this.setState({ loading: true });
+    const getSong = musicArray.filter((elemt) => (elemt.trackName === favSong.name));
+    await addSong(getSong);
+    this.setState({ loading: false });
+  }
+
   render() {
-    const { musicArray, colectionName, artistName } = this.state;
+    const { musicArray, colectionName, artistName, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { loading && <Loading /> }
         <h3 data-testid="artist-name">{artistName}</h3>
         <h4 data-testid="album-name">{colectionName}</h4>
         <section>
@@ -41,6 +53,16 @@ class Album extends React.Component {
           { musicArray.filter((item) => item.trackId).map((item) => (
             <div key={ item.trackNumber }>
               <p>{item.trackName}</p>
+              <label htmlFor={ item.trackId }>
+                Favorita
+                <input
+                  id={ item.trackId }
+                  type="checkbox"
+                  data-testid={ `checkbox-music-${item.trackId}` }
+                  onChange={ this.getTarget }
+                  name={ item.trackName }
+                />
+              </label>
               <audio data-testid="audio-component" src={ item.previewUrl } controls>
                 <track kind="captions" />
                 O seu navegador não suporta o elemento

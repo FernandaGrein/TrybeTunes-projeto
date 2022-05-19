45a9b76d7ec97acd34af4e5face11c613a5fda2d
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-19 10:31:07 -0300

    adiciona requisito 6

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,31 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import Loading from '../Components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     bandName: '',
     disabled: true,
+    loading: false,
+    saveName: '',
+    apiResponse: [],
   }
 
   getBandName = (event) => {
     const { value } = event.target;
     const minValue = 2;
-    this.setState({ bandName: value }, () => (
+    this.setState({ bandName: value, saveName: value }, () => (
       value.length >= minValue ? this.setState({ disabled: false }) : (
         this.setState({ disabled: true })
       )));
   }
 
+  handleClick = async () => {
+    const { saveName } = this.state;
+    this.setState({ bandName: '', loading: true });
+    const albuns = await searchAlbumsAPI(saveName);
+    this.setState({ apiResponse: albuns, loading: false });
+  }
+
   render() {
-    const { bandName, disabled } = this.state;
+    const { bandName, disabled, loading, apiResponse, saveName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        { loading && <Loading /> }
+        { apiResponse.length > 0 ? (<div>
+          <p>{`Resultado de álbuns de: ${saveName}`}</p>
+          {apiResponse.map((album) => (
+            <div key={ album.collectionId }>
+              <p>{album.artistId}</p>
+              <p>{album.artistName}</p>
+              <p>{album.collectionId}</p>
+              <p>{album.collectionName}</p>
+              <p>{album.collectionPrice}</p>
+              <Link
+                data-testid={ `link-to-album-${album.collectionId}` }
+                to={ `/album/${album.collectionId}` }
+              >
+                Album
+              </Link>
+              <img src={ album.artworkUrl100 } alt={ album.artistName } />
+              <p>{album.trackCount}</p>
+            </div>
+          ))}
+        </div>) : (<p>Nenhum álbum foi encontrado</p>)}
         <form>
-          <label htmlFor="name">
+          <label htmlFor="bandName">
             Nome:
             <input
-              id="name"
+              id="bandName"
               type="text"
               value={ bandName }
               data-testid="search-artist-input"
@@ -33,9 +67,10 @@ class Search extends React.Component {
             />
           </label>
           <button
-            type="submit"
+            type="button"
             data-testid="search-artist-button"
             disabled={ disabled }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
